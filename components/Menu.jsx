7c906618d0ec5f740dd92f7eff576977e6355817
Dragan Rapić <82722626+dragan1011@users.noreply.github.com>
2022-11-02 15:54:07 +0100

    Add files via upload

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -8,10 +8,14 @@ import "aos/dist/aos.css"; */
 import "swiper/css";
 
 function Menu() {
-  const [menu, setMenu] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-  const handleMenu = () => {
-    setMenu(!menu);
+  const handleMenu = (key) => {
+    setIsActive(!isActive);
+  };
+
+  const onRowClick = (e, clickedRow) => {
+    console.log(clickedRow);
   };
 
   /*  useEffect(() => {
@@ -31,7 +35,10 @@ function Menu() {
         <div className="max-w-[1000px] relative ml-auto mr-auto ">
           <Swiper spaceBetween={0} slidesPerView={"auto"}>
             <SwiperSlide
-              /* data-aos="fade-up" */
+              onRowClick={onRowClick}
+              data-aos="fade-up"
+              onClick={handleMenu}
+              style={{ backgroundColor: isActive ? "#555555" : "#EAD18596" }}
               className="max-w-[200px] text-center items-center justify-center border-x border-[#FFFFFF10] bg-[#EAD18596] py-5 px-10 max-h-[100px] hover:bg-[#EAD18596] duration-300 cursor-pointer"
             >
               <img
@@ -43,8 +50,9 @@ function Menu() {
             </SwiperSlide>
             <SwiperSlide
               onClick={handleMenu}
-              /* data-aos="fade-up"
-              data-aos-delay="100" */
+              style={{ backgroundColor: isActive ? "#EAD18596" : "#555555" }}
+              data-aos="fade-up"
+              data-aos-delay="100"
               className="max-w-[200px] text-center items-center justify-center border-x border-[#FFFFFF10] bg-[#555555] py-6 px-10 max-h-[100px] hover:bg-[#EAD18596] duration-300 cursor-pointer"
             >
               <img
@@ -55,8 +63,11 @@ function Menu() {
               <span>Wine</span>
             </SwiperSlide>
             <SwiperSlide
-              /* data-aos="fade-up"
-              data-aos-delay="200" */
+              key={2}
+              onClick={handleMenu}
+              style={{ backgroundColor: isActive ? "#EAD18596" : "#555555" }}
+              data-aos="fade-up"
+              data-aos-delay="200"
               className="max-w-[200px] text-center items-center justify-center border-x border-[#FFFFFF10] bg-[#555555] py-5 px-10 max-h-[100px] hover:bg-[#EAD18596] duration-300 cursor-pointer"
             >
               <img
@@ -67,8 +78,11 @@ function Menu() {
               <span>Coctails</span>
             </SwiperSlide>
             <SwiperSlide
-              /* data-aos="fade-up"
-              data-aos-delay="300" */
+              key={3}
+              onClick={handleMenu}
+              style={{ backgroundColor: isActive ? "#EAD18596" : "#555555" }}
+              data-aos="fade-up"
+              data-aos-delay="300"
               className="max-w-[200px] text-center items-center justify-center border-x border-[#FFFFFF10] bg-[#555555] py-6 px-5 max-h-[100px] hover:bg-[#EAD18596] duration-300 cursor-pointer"
             >
               <img
@@ -79,8 +93,11 @@ function Menu() {
               <span>Non-alcoholic</span>
             </SwiperSlide>
             <SwiperSlide
-              /*  data-aos="fade-up"
-              data-aos-delay="400" */
+              key={4}
+              onClick={handleMenu}
+              style={{ backgroundColor: isActive ? "#EAD18596" : "#555555" }}
+              data-aos="fade-up"
+              data-aos-delay="400"
               className="max-w-[200px] text-center items-center justify-center border-x border-[#FFFFFF10] bg-[#555555] py-6 px-10] max-h-[100px] hover:bg-[#EAD18596] duration-300 cursor-pointer"
             >
               <img
@@ -92,76 +109,78 @@ function Menu() {
             </SwiperSlide>
           </Swiper>
         </div>
-        <div className="ml-[15rem] grid grid-cols-2 max-[1250px]:grid-cols-1 max-md:ml-[6rem] max-small-tablet:ml-[5rem] max-big-phone:ml-[3rem] max-small-phone:ml-[1rem] ">
-          <div /* data-aos="fade-up" */ className="grid grid-cols-2 gap-4">
-            <div className="flex flex-col">
-              <Span>Heißgetränke</Span>
-              <P>Verlängerter</P>
-              <P>Cappuccino</P>
-              <P>Caffe Latte</P>
-              <P>Kleiner Brauner</P>
-              <P>Großer Brauner</P>
-              <P>Espresso</P>
-              <P>Tee div, Sorten</P>
+        {!isActive && (
+          <div className="ml-[15rem] grid grid-cols-2 max-[1250px]:grid-cols-1 max-md:ml-[6rem] max-small-tablet:ml-[5rem] max-big-phone:ml-[3rem] max-small-phone:ml-[1rem] ">
+            <div /* data-aos="fade-up" */ className="grid grid-cols-2 gap-4">
+              <div className="flex flex-col">
+                <Span>Heißgetränke</Span>
+                <P>Verlängerter</P>
+                <P>Cappuccino</P>
+                <P>Caffe Latte</P>
+                <P>Kleiner Brauner</P>
+                <P>Großer Brauner</P>
+                <P>Espresso</P>
+                <P>Tee div, Sorten</P>
+              </div>
+              <div className="flex flex-col max-md:ml-[7rem] max-small-tablet:ml-[5rem] max-phone:ml-[3rem]">
+                <P></P>
+                <P></P>
+                <div className="mt-[1.5rem] max-mid-phone:mt-[2rem]"></div>
+                <P>2,90 €</P>
+                <P>3.20 €</P>
+                <P>3.50 €</P>
+                <P>2.60 €</P>
+                <P>3.90 €</P>
+                <P>2.60 €</P>
+                <P>2.60 €</P>
+              </div>
             </div>
-            <div className="flex flex-col max-md:ml-[7rem] max-small-tablet:ml-[5rem] max-phone:ml-[3rem]">
-              <P></P>
-              <P></P>
-              <div className="mt-[1.5rem] max-mid-phone:mt-[2rem]"></div>
-              <P>2,90 €</P>
-              <P>3.20 €</P>
-              <P>3.50 €</P>
-              <P>2.60 €</P>
-              <P>3.90 €</P>
-              <P>2.60 €</P>
-              <P>2.60 €</P>
+            <div /* data-aos="fade-up" */ className="grid grid-cols-2 gap-4 ">
+              <div className="flex flex-col">
+                <Span>Bio-Limonaden</Span>
+                <P>Lemon Prickelnd</P>
+                <P>Lemon Still</P>
+                <P>Kirsche Prickelnd</P>
+                <P>Kirsche Still</P>
+                <P>Gruntee-Kaktusfeige Prickelnd</P>
+                <P>Gruntee-Kaktusfeige Still</P>
+              </div>
+              <div className="flex flex-col max-md:ml-[7rem] max-small-tablet:ml-[5rem] max-phone:ml-[3rem]">
+                <P></P>
+                <P></P>
+                <div className="mt-[1.5rem]"></div>
+                <P>4.20€</P>
+                <P>3.90 €</P>
+                <P>4.20 €</P>
+                <P>3.90 €</P>
+                <P>4.20 €</P>
+                <P>3.90 €</P>
+              </div>
+            </div>
+            <div
+              /* data-aos="fade-up" */ className="grid grid-cols-2 gap-4 mt-10"
+            >
+              <div className="flex flex-col">
+                <Span>Soda, Mineral</Span>
+                <P>Römerquelle Prickelnd</P>
+                <P>Römerquelle Still</P>
+                <P>Soda Zitrone</P>
+              </div>
+              <div className="flex flex-col max-md:ml-[7rem] max-small-tablet:ml-[5rem] max-phone:ml-[3rem]">
+                <P></P>
+                <P></P>
+                <div className="mt-[1.5rem]"></div>
+                <P>2,90 €</P>
+                <P>2,90 €</P>
+                <P>2,90 €</P>
+              </div>
             </div>
           </div>
-          <div /* data-aos="fade-up" */ className="grid grid-cols-2 gap-4 ">
-            <div className="flex flex-col">
-              <Span>Bio-Limonaden</Span>
-              <P>Lemon Prickelnd</P>
-              <P>Lemon Still</P>
-              <P>Kirsche Prickelnd</P>
-              <P>Kirsche Still</P>
-              <P>Gruntee-Kaktusfeige Prickelnd</P>
-              <P>Gruntee-Kaktusfeige Still</P>
-            </div>
-            <div className="flex flex-col max-md:ml-[7rem] max-small-tablet:ml-[5rem] max-phone:ml-[3rem]">
-              <P></P>
-              <P></P>
-              <div className="mt-[1.5rem]"></div>
-              <P>4.20€</P>
-              <P>3.90 €</P>
-              <P>4.20 €</P>
-              <P>3.90 €</P>
-              <P>4.20 €</P>
-              <P>3.90 €</P>
-            </div>
-          </div>
-          <div
-            /* data-aos="fade-up" */ className="grid grid-cols-2 gap-4 mt-10"
-          >
-            <div className="flex flex-col">
-              <Span>Soda, Mineral</Span>
-              <P>Römerquelle Prickelnd</P>
-              <P>Römerquelle Still</P>
-              <P>Soda Zitrone</P>
-            </div>
-            <div className="flex flex-col max-md:ml-[7rem] max-small-tablet:ml-[5rem] max-phone:ml-[3rem]">
-              <P></P>
-              <P></P>
-              <div className="mt-[1.5rem]"></div>
-              <P>2,90 €</P>
-              <P>2,90 €</P>
-              <P>2,90 €</P>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
 
       <div className="text-center">
-        {setMenu && <P>We don't have any data in this moment.</P>}
+        {isActive && <P>We don't have any data in this moment.</P>}
       </div>
     </div>
   );
